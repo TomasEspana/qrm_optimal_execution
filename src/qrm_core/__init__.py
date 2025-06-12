6eb7c35d4d3d31b4f2e800fe662b2f05cd92e6a0
Tomas <tomas.espana@student-cs.fr>
2025-06-12 17:07:09 +0200

    final check for qrm_core

--- src/qrm_core/__init__.py
+++ src/qrm_core/__init__.py
@@ -1,5 +1,5 @@
 from .engine import simulate_QRM_jit
 from .intensity import IntensityTable
-from .invariant import compute_invariant_distribution
+from .invariant_distribution import compute_invariant_distribution
 from .runner import simulate_QRM
 from .sampling import sample_stationary_lob, choose_next_event, update_LOB
