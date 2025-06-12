d4203746050849fc8bfd5c17e4bffbf7f2da3b03
Tomas <tomas.espana@student-cs.fr>
2025-06-12 17:27:35 +0200

    agent init file update

---
+++ src/qrm_rl/agents/__init__.py
@@ -0,0 +1,4 @@
+from .benchmark_strategies import TWAPAgent, BackLoadAgent, FrontLoadAgent
+from .ddqn import DDQNAgent
+from .network import DQNNetwork
+from .replay import ReplayMemory, Transition
--- src/qrm_rl/agents/benchmark_strategies.py
+++ src/qrm_rl/agents/benchmark_strategies.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 """ 
     Benchmark trading strategies.
 """
