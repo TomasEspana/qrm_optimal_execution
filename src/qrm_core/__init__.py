a6538e1863ef333a39f45049878efe8d59a6a929
Tomas España <tomashimalaya@gmail.com>
2025-12-31 15:59:37 +0100

    modify __init__.py

--- src/qrm_core/__init__.py
+++ src/qrm_core/__init__.py
@@ -1,5 +1,4 @@
 from .engine import simulate_QRM_jit
 from .intensity import IntensityTable
 from .invariant_distribution import compute_invariant_distribution
-from .depr_runner import simulate_QRM
-from .sampling import sample_stationary_lob, choose_next_event_min, update_LOB
+from .sampling import sample_stationary_lob, choose_next_event, update_LOB
